cbea08c6c9bb2b4e3b817dbc8c40f76399f95ff2
officialdhruvv <103775416+officialdhruvv@users.noreply.github.com>
2023-09-05 16:19:18 +0000

    Conversions of Datatypes

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,49 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"= 33
+// "33abc" = NaN
+// true = 1; flase = 0
+
+let isloggedIn = "Dhruv"
+
+console.log(typeof isloggedIn);
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+// 1 = true
+// 0 =false
+// "" = false
+// "Dhruv" = ture
