3766b864bf185f20819309ec8fb68b7db57efb98
officialdhruvv <103775416+officialdhruvv@users.noreply.github.com>
2023-09-05 16:19:03 +0000

    Data Types

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3+3) // We are using nodejs, not in browser
+
+//console.log(3+3) console.log("Dhruv") // Code Readability should High Pirorty
+
+//ECMA Script it writes the standards tc39.es
+
+let name = "Dhruv" //string
+let age = 18 
+let isLoggedIn = false // boolean
+let state;
+//null is standlone value
+//undefined = means the varibale is empty
+//symbol = unique value
+
+
+// object
