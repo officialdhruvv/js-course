ef514ac401f72e8aa2f2ea9531e10e0b1db2249e
officialdhruvv <103775416+officialdhruvv@users.noreply.github.com>
2023-09-06 14:24:06 +0000

    Operations

---
+++ .devcontainer/01_basics/04_operations.js
@@ -0,0 +1,27 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**5);
+// console.log(3/3);
+// console.log(3%3);
+
+// let str1 = "Dhruv"
+// let str2 = "Sharma"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console/log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 num1 num1 num1 
