8e581a94d8487a946652340fb58d26c765d59f43
officialdhruvv <103775416+officialdhruvv@users.noreply.github.com>
2023-09-18 13:50:45 +0000

    Date in JS

---
+++ .devcontainer/01_basics/07_dateinJs.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0 , 25)
+// let myCreateDate = new Date (2023, 0, 23, 5, 3)
+// let myCreateDate = new Date ("2023-01-14")
+ let myCreateDate = new Date ("01-14-2023")
+// console.log(myCreateDate);
+// console.log(myCreateDate.toString());
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
